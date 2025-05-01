d2d2ccec9c2df3d23568673b7def900df4a5bbfe
Onedes19 <105841103820@student.unismuh.ac.id>
2025-05-01 22:02:56 +0800

    Aplikasi RasaGula

--- rasagula/settings.py
+++ rasagula/settings.py
@@ -117,6 +117,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'  # URL prefix untuk file statis
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Lokasi folder statis di produksi
 
 STATICFILES_DIRS = [
